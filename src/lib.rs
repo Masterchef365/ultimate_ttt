c0a736c2a1fb7017d949a45de3e2aae72cfc7758
Masterchef365 <duncan.freeman1@gmail.com>
2021-11-20 16:53:56 -0800

    Untested successor and move application

--- src/lib.rs
+++ src/lib.rs
@@ -10,28 +10,96 @@ pub type Board = [Square; 9];
 /// Row-major board of boards
 pub type SuperBoard = [Board; 9];
 
-struct GameSetup {
+pub struct GameSetup {
     /// The players in this game, ordered by who goes first.
     players: Vec<Player>,
 }
 
-struct Move {
+#[derive(Copy, Clone, Debug)]
+pub struct Move {
     /// If the user has the choice to pick the superboard square of their next move, this is Some(index) and otherwise None
     superboard: Option<usize>,
-    /// Which square of the sub-board to make the move to 
+    /// Which square of the sub-board to make the move to
     board: usize,
 }
 
-struct GameState {
+#[derive(Copy, Clone, Debug)]
+pub struct GameState {
     /// The board state
-    board: SuperBoard,
+    superboard: SuperBoard,
     /// Index of the player from GameSetup::players who will make the next move
     next_to_play: usize,
+    /// If any, the index of the superboard square the player has been sent to.
+    sent_to: Option<usize>,
 }
 
 /// Return the successors of the current board state. Will return an empty vector if the game is finished.
-pub fn successors(state: &GameState, setup: &GameSetup) -> Vec<Move> {
-    
+pub fn successors(state: &GameState) -> Vec<Move> {
+    if let Some(send) = state.sent_to {
+        open_board_squares(state.superboard[send])
+            .map(|square| Move {
+                superboard: None,
+                board: square,
+            })
+            .collect()
+    } else {
+        state
+            .superboard
+            .iter()
+            .enumerate()
+            .map(|(superboard_idx, board)| {
+                open_board_squares(*board).map(move |square| Move {
+                    superboard: Some(superboard_idx),
+                    board: square,
+                })
+            })
+            .flatten()
+            .collect()
+    }
+}
+
+impl GameState {
+    pub fn apply_move(&self, mov: Move, setup: &GameSetup) -> Self {
+        // Check if the superboard move is legal
+        let board_idx = match (self.sent_to, mov.superboard) {
+            (Some(i), None) | (None, Some(i)) => i,
+            (Some(_), Some(_)) => panic!("Move included superboard index when not needed"),
+            (None, None) => panic!("Move did not include superboard index when needed"),
+        };
+
+        // Check if the board move is legal
+        let mut board = self.superboard[board_idx];
+        if board[mov.board].is_some() {
+            panic!("Illegal move in sub-board {}; {}", board_idx, mov.board);
+        }
+        
+        // Make the move
+        let player = setup.players[self.next_to_play];
+        board[mov.board] = Some(player);
+
+        let mut superboard = self.superboard;
+        superboard[board_idx] = board;
+
+        // Determine if the next player is sent
+        let sent_to = open_board_squares(superboard[mov.board]).next().is_some().then(|| mov.board);
+
+        // Calculate the next player
+        let next_to_play = (self.next_to_play + 1) % setup.players.len();
+
+        Self {
+            superboard,
+            next_to_play,
+            sent_to,
+        }
+    }
+}
+
+/// Returns an iterator over the squares yet to be populated in this board
+pub fn open_board_squares(board: Board) -> impl Iterator<Item = usize> {
+    board
+        .into_iter()
+        .enumerate()
+        .filter_map(|(idx, square)| (square == Square::None).then(|| idx))
 }
 
 pub fn row_is_won(row: [Square; 3]) -> Option<Player> {
