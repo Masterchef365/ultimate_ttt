499fb75c9a00a169ea28b4cfaf8493e50cebf9be
Masterchef365 <duncan.freeman1@gmail.com>
2021-12-01 22:11:17 -0800

    Stats

--- src/bin/explore_gametree.rs
+++ src/bin/explore_gametree.rs
@@ -1,5 +1,5 @@
-use std::{collections::HashMap};
-use ultimate_ttt::{open_board_squares, Board, EMPTY_BOARD, SuperBoard, GameState, print_superboard, EMPTY_SUPERBOARD, GamePrintGuides, successors};
+use std::{collections::HashMap, time::Instant};
+use ultimate_ttt::{open_board_squares, Board, EMPTY_BOARD, SuperBoard, GameState, print_superboard, EMPTY_SUPERBOARD, GamePrintGuides, successors, is_superboard_won};
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 struct SingleBoardState {
@@ -44,6 +44,12 @@ fn main() {
     let mut tree = HashMap::new();
     let mut queue = vec![GameState::new(b"XO")];
     let mut i = 0;
+    let mut complete_games = 0;
+    let mut o_wins = 0;
+    let mut x_wins = 0;
+
+
+    let start = Instant::now();
     while let Some(state) = queue.pop() {
         //print_superboard(&state.superboard, Some(GamePrintGuides::Superboard));
         //println!("--------------------------------------------------");
@@ -55,9 +61,22 @@ fn main() {
             queue.extend(successor_states.iter().copied());
             tree.insert(state, successor_states);
         }
+
+        if is_superboard_won(&state.superboard).is_some() {
+            complete_games += 1;
+        }
+
+        match is_superboard_won(&state.superboard) {
+            Some(b'X') => x_wins += 1,
+            Some(b'O') => o_wins += 1,
+            _ => (),
+        }
+
         i += 1;
         if i % 10_000 == 0 {
-            println!("{}", i);
+            let elapsed = start.elapsed().as_secs_f32();
+            let rate = i as f32 / elapsed;
+            println!("{:2} / sec, idx: {}, tree: {}, complete: {}, O/X: {}/{}", rate, i, tree.len(), complete_games, o_wins, x_wins);
         }
     }
 }
